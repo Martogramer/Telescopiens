e66be1ab97a88054936d4a73c51657b45f4d101a
Marto <contact@torox.com>
2022-03-31 20:55:27 -0300

    headerjsx

--- portfolio-martiniribas/martogramer/src/components/header/Header.jsx
+++ portfolio-martiniribas/martogramer/src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import styles from './header.css';
+import CTA from './CTA';
+//import ME from '../../assets'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
     return(
-        <div class={styles.header}><h1>header</h1></div>
+        
+        <header>
+            <div className="container header__container">
+                <h5>Hola soy</h5>
+                <h1>Martin</h1>
+                <h5 className="text-light">Fullstack Developer</h5>
+                <CTA />
+                <HeaderSocials />
+
+                <div className="me">
+                    <img src="" alt="" />
+                </div>
+
+                <a href="#contact" className='scroll__down'>Scroll Down</a>
+            </div>
+        </header>
+        
         ) 
         
 };
