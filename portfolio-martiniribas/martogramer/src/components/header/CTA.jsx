ce502ca30932bec54fe95498944ee74844dff0dd
Marto <contact@torox.com>
2022-03-31 20:53:01 -0300

    cta

---
+++ portfolio-martiniribas/martogramer/src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+//import CV from '../../assets'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href='#' download className='btn btn-primary'>Download CV</a>
+        <a href="#contact" className='btn btn-primary'>Contactame</a>
+    </div>
+  )
+}
+
+export default CTA
