334ee53e93181e1fba0ce5062fcec114784b27a7
Martin <martin.iribass@gmail.com>
2022-04-04 11:04:23 -0300

    about

--- portfolio-martiniribas/martogramer/src/components/about/About.jsx
+++ portfolio-martiniribas/martogramer/src/components/about/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './about.css'
+import ME from '../../assets/me.jpg';
+
 
 const About = () => {
     return(
@@ -10,12 +12,25 @@ const About = () => {
             <div className="container about__container">
                 <div className="about__me">
                     <div className="about__me-image">
-                        <img src="" alt="" />
+                        <img src={ME} alt="" />
                     </div>
                 </div>
 
                 <div className="about__content">
-                    
+                    <div className="about__cards">
+                        <article className="about__card">
+                            <h5>Experiece</h5>
+                            <small>1+ years working</small>
+                        </article>
+                        <article className="about__card">
+                            <h5>Projects</h5>
+                            <small>20+ projects in board</small>
+                        </article>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam cupiditate, ducimus dolore culpa, consequuntur non libero commodi magni soluta quo ipsum ratione perspiciatis deserunt quas fuga pariatur optio nobis deleniti!
+                    </p>
+                    <a href="#contact" className="btn btn-primary">Contact</a>
                 </div>
             </div>
         </section>
