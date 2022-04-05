8a118e9e3e6d5f4d41f2a12df6807593b0e59dfb
Martin <martin.iribass@gmail.com>
2022-04-05 09:28:10 -0300

    services

--- portfolio-martiniribas/martogramer/src/components/services/Services.jsx
+++ portfolio-martiniribas/martogramer/src/components/services/Services.jsx
@@ -4,7 +4,77 @@ import './services.css'
 const Services = () => {
     return(
         <section id="services">
-            Services
+            <h5>What i offer</h5>
+            <h2>Services</h2>
+
+            <div className="container services__container">
+                <article className="service">
+                    <div className="services__head">
+                        <h3>UI/UX design</h3>
+                    </div>
+                    <ul className="services__list">
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                    </ul>
+                </article>
+                <article className="service">
+                    <div className="services__head">
+                        <h3>Frontend</h3>
+                    </div>
+                    <ul className="services__list">
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                    </ul>
+                </article>
+                <article className="service">
+                    <div className="services__head">
+                        <h3>Backend</h3>
+                    </div>
+                    <ul className="services__list">
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
             </section>
         ) 
         
