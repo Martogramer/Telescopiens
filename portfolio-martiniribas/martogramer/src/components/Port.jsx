1ebb3365e6331b320d8146438d340ecda75e28b6
Martin <martin.iribass@gmail.com>
2022-04-07 17:48:23 -0300

    final

--- portfolio-martiniribas/martogramer/src/components/Port.jsx
+++ portfolio-martiniribas/martogramer/src/components/Port.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import CTA from './header/CTA'
 import './port.css';
 import pro from '../assets/pro.jpg'
+import ProductList from './productList/ProductList';
+import Contact from './contact/Contact';
+import {BsWhatsapp} from 'react-icons/bs';
+import {AiOutlineMail} from 'react-icons/ai';
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
 
 
 const Port=()=> {
     return (
+        <div>
+
+            <div className='navetones'>
+                <nav className="nodriza">
+                    <a href="https://www.linkedin.com/in/martogramer/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+                    <a href="https://www.github.com/martogramer/" target="_blank" rel="noreferrer"><BsGithub/></a>
+                    <a href="https://wa.me/5493492524734/?text=Hola%20quisiera%20contratar%20tu%20servicio" target="_blank" rel="noreferrer"><BsWhatsapp /></a>
+                    <a href="mailto:martin.iribass@gmail.com" target="_blank" rel="noreferrer"><AiOutlineMail /></a>
+                </nav>
+            </div>
             <div className="container_">
-                
                 <div className="header">
                     <div className="header__content">
                         <div className="foto">
@@ -15,20 +30,20 @@ const Port=()=> {
                         </div>
                         <div>
                             <div>
-                                <h5>Hola soy</h5>
+                                <p>👋Hola soy</p>
                                 <h1>Martin</h1>
-                                <h5>Fullstack Developer</h5>
+                                <p>Fullstack Developer</p>
                             </div>
                             <CTA />
                         </div>
                     </div>
                 </div>
-
+                <ProductList/>
                 <div>
-                        <div className="about-hola">
-                            <h5>Get to know</h5>
-                            <h1>About Me</h1>
-                        </div>
+                    <div className="about-hola">
+                        <h5>Get to know</h5>
+                        <h1>About Me</h1>
+                    </div>
                     <div className="about">
                         <div className="skills">
                             <div><h3>Skills</h3>
@@ -56,7 +71,7 @@ const Port=()=> {
                     </div>
                     <div className="portfolio-content">
                         <div className="items">
-                            <div className="items-img"><img src={pro} alt="" /></div>
+                            <div><img src={pro} alt="" className="items-img"/></div>
                             <h3>titulo del project</h3>
                             <div className="cuadro">
                                 <a href="" className="btn">Github</a>
@@ -64,7 +79,7 @@ const Port=()=> {
                             </div>
                         </div>
                         <div className="items">
-                            <div className="items-img"><img src={pro} alt="" /></div>
+                            <div><img src={pro} alt="" className="items-img"/></div>
                             <h3>titulo del project</h3>
                             <div className="cuadro">
                                 <a href="" className="btn">Github</a>
@@ -72,7 +87,7 @@ const Port=()=> {
                             </div>
                         </div>
                         <div className="items">
-                            <div className="items-img"><img src={pro} alt="" /></div>
+                            <div><img src={pro} alt="" className="items-img"/></div>
                             <h3>titulo del project</h3>
                             <div className="cuadro">
                                 <a href="" className="btn">Github</a>
@@ -80,7 +95,7 @@ const Port=()=> {
                             </div>
                         </div>
                         <div className="items">
-                            <div className="items-img"><img src={pro} alt="" /></div>
+                            <div><img src={pro} alt="" className="items-img"/></div>
                             <h3>titulo del project</h3>
                             <div className="cuadro">
                                 <a href="" className="btn">Github</a>
@@ -88,7 +103,7 @@ const Port=()=> {
                             </div>
                         </div>
                         <div className="items">
-                            <div className="items-img"><img src={pro} alt="" /></div>
+                            <div><img src={pro} alt="" className="items-img"/></div>
                             <h3>titulo del project</h3>
                             <div className="cuadro">
                                 <a href="" className="btn">Github</a>
@@ -96,7 +111,7 @@ const Port=()=> {
                             </div>
                         </div>
                         <div className="items">
-                            <div className="items-img"><img src={pro} alt="" /></div>
+                            <div><img src={pro} alt="" className="items-img"/></div>
                             <h3>titulo del project</h3>
                             <div className="cuadro">
                                 <a href="" className="btn">Github</a>
@@ -104,12 +119,13 @@ const Port=()=> {
                             </div>
                         </div>
                         
-
                     </div>
                 </div>
-
+                
+                <Contact/>
 
             </div>
+        </div>
     )
 }
 
